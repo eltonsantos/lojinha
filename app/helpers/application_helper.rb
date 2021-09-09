f1a7658e851e7995706a7caad52f274ef9cf480e
Elton Santos <eltonaxl@hotmail.com>
2021-09-08 22:13:28 -0300

    Flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_message
+    messages = ""
+    [:notice, :info, :warning, :error].each { |type|
+      if flash[type]
+        messages += "<div class='base-alert #{type}' role='alert'>#{flash[type]}</div>"
+      end
+    }
+
+    messages.html_safe
+  end
 end
